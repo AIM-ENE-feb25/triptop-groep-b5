31dd377ebb9ba73aed74ae17d3382566ece753a7
SJWelles <sj.welles@student.han.nl>
2025-03-21 09:45:19 +0100

    Een poging voor die prototype

--- week1/les3/prototype/prototype-google-maps/src/App.jsx
+++ week1/les3/prototype/prototype-google-maps/src/App.jsx
@@ -2,20 +2,67 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [search, setSearch] = useState('')
+  const [results, setResults] = useState({})
+
+  const query = () => {
+    const url = 'https://google-map-places-new-v2.p.rapidapi.com/v1/places:searchText';
+    const options = {
+      method: 'POST',
+      headers: {
+        'x-rapidapi-key': '262b586635msh92707a41bb69d42p16a7eajsn1d5caaa53bfa',
+        'x-rapidapi-host': 'google-map-places-new-v2.p.rapidapi.com',
+        'Content-Type': 'application/json',
+        'X-Goog-FieldMask': '*'
+      },
+      body: JSON.stringify({
+        textQuery: 'restaurants',
+        languageCode: '',
+        regionCode: '',
+        rankPreference: 0,
+        includedType: '',
+        openNow: true,
+        minRating: 0,
+        maxResultCount: 1,
+        priceLevels: [],
+        strictTypeFiltering: true,
+        locationBias: {
+          circle: {
+            center: {
+              latitude: 40,
+              longitude: -110
+            },
+            radius: 10000
+          }
+        },
+        evOptions: {
+          minimumChargingRateKw: 0,
+          connectorTypes: []
+        }
+      })
+    };
+
+    fetch(url, options)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setResults(data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
 
   return (
     <>
-      <iframe
-        title="Google Maps view of Space Needle, Seattle WA"
-        width="600"
-        height="450"
-        style={{ border: 0 }}
-        loading="lazy"
-        allowFullScreen
-        referrerPolicy="no-referrer-when-downgrade"
-        src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCYXC-hg8uekGhzT5tyS3jhal7sL-2RVSw&q=Space+Needle,Seattle+WA">
-      </iframe>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={search}
+        onChange={(event) => setSearch(event.target.value)}
+      />
+      <button onClick={() => query(search)}>Search</button>
+      {/* <p>Results: {results}</p> */}
     </>
   )
 }
