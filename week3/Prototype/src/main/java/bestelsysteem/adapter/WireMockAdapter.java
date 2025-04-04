8dea1e854f93918d501322883f27a64a2539ff28
CozyLeprechaun94 <44181698+CozyLeprechaun94@users.noreply.github.com>
2025-04-04 11:56:52 +0200

    implementatie

--- week3/Prototype/src/main/java/bestelsysteem/adapter/WireMockAdapter.java
+++ week3/Prototype/src/main/java/bestelsysteem/adapter/WireMockAdapter.java
@@ -1,62 +1,41 @@
 package bestelsysteem.adapter;
 
 import bestelsysteem.exception.APICallException;
+import bestelsysteem.external.WireMockDriver;
 import bestelsysteem.model.ErrorObject;
 import bestelsysteem.model.TokenObject;
 import bestelsysteem.model.UserAccessInfo;
 import bestelsysteem.model.UserAuthorization;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientResponseException;
-import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
-import java.util.Collections;
 
 @Service
 public class WireMockAdapter implements AuthAdapter {
+    private final WireMockDriver wireMockAPI;
+    private final ObjectMapper objectMapper;
 
-    @Override
-    public String getToken(UserAuthorization userAuthorization) {
-        try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
-
-            HttpEntity<UserAuthorization> request = new HttpEntity<>(userAuthorization, headers);
-            RestTemplate restTemplate = new RestTemplate();
-            String resourceUrl = "http://localhost:8080/login";
-            String response =
-                    restTemplate.postForObject(resourceUrl, request, String.class);
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            TokenObject tokenObject = objectMapper.readValue(response, TokenObject.class);
-            return tokenObject.getToken();
-        } catch (RestClientResponseException e) {
-            throw new APICallException(new ErrorObject(e.getMessage(), e.getClass().getSimpleName(), "Authentication provider is current unavailable. Try again later."));
-        } catch (IOException e) {
-            throw new APICallException(new ErrorObject(e.getMessage(), e.getClass().getSimpleName(), "Unable to get token. Try again later."));
-        }
+    public WireMockAdapter() {
+        wireMockAPI = new WireMockDriver();
+        objectMapper = new ObjectMapper();
     }
 
-    @Override
     public UserAccessInfo getRole(UserAuthorization userAuthorization) {
         try {
-            HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            headers.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
+            String loginResponse = wireMockAPI.login(userAuthorization.getUsername(), userAuthorization.getPassword());
+            TokenObject tokenObject = objectMapper.readValue(loginResponse, TokenObject.class);
+            String token = tokenObject.getToken();
+            String checkAppAccessResponse = wireMockAPI.checkAppAccess(userAuthorization.getUsername(), userAuthorization.getApplication(), token);
 
-            HttpEntity<UserAuthorization> request = new HttpEntity<>(userAuthorization, headers);
-            RestTemplate restTemplate = new RestTemplate();
-            String resourceUrl = "http://localhost:8080/checkAppAccess?token=" + userAuthorization.getToken();
+            UserAccessInfo userAccessInfo = objectMapper.readValue(checkAppAccessResponse, UserAccessInfo.class);
+            userAccessInfo.setToken(token);
 
-            String response = restTemplate.postForObject(resourceUrl, request, String.class);
 
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(response, UserAccessInfo.class);
+            return userAccessInfo;
         } catch (RestClientResponseException e) {
-            throw new APICallException(new ErrorObject(e.getMessage(), e.getClass().getSimpleName(), "Authentication provider is current unavailable. Try again later."));
+            throw new APICallException(new ErrorObject(e.getMessage(), e.getClass().getSimpleName(), "Username or password is incorrect."));
         } catch (IOException e) {
             throw new APICallException(new ErrorObject(e.getMessage(), e.getClass().getSimpleName(), "Unable to get token. Try again later."));
         }
--- week3/Prototype/src/main/java/bestelsysteem/controller/GatewayController.java
+++ week3/Prototype/src/main/java/bestelsysteem/controller/GatewayController.java
@@ -1,9 +1,12 @@
 package bestelsysteem.controller;
 
+import bestelsysteem.adapter.WireMockAdapter;
+import bestelsysteem.model.UserAccessInfo;
 import bestelsysteem.model.UserAuthorization;
 import bestelsysteem.service.port.AuthServicePort;
 import bestelsysteem.service.port.AuthorizationServicePort;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,38 +21,52 @@ import java.net.http.HttpResponse;
 public class GatewayController {
     private final AuthorizationServicePort authorizationServicePort;
     private final AuthServicePort authServicePort;
+    private final WireMockAdapter wireMockAdapter;
+
+    @Value("${rapidapi.key}")
+    private String tokenRapid;
+
+    @Value("${rapidapi.host}")
+    private String hostRapid;
 
     @Autowired
-    public GatewayController(AuthorizationServicePort authorizationServicePort, AuthServicePort authServicePort) {
+    public GatewayController(AuthorizationServicePort authorizationServicePort, AuthServicePort authServicePort, WireMockAdapter wireMockAdapter) {
         this.authorizationServicePort = authorizationServicePort;
         this.authServicePort = authServicePort;
+        this.wireMockAdapter = wireMockAdapter;
     }
 
     @GetMapping("/request")
     public ResponseEntity<String> handleRequest(@RequestHeader("Authorization") String token) {
-        UserAuthorization userAuthorization = new UserAuthorization();
-        userAuthorization.setToken(token);
-        userAuthorization.setApplication("triptop");
-
-        String authorizedUser = String.valueOf(authServicePort.authorizeUser(userAuthorization));
-        boolean hasAccess = authorizationServicePort.hasAccess(token, "klant");
-
-        if (hasAccess && authorizedUser != null) {
-            try {
-                HttpRequest request = HttpRequest.newBuilder()
-                        .uri(URI.create("https://sky-scanner3.p.rapidapi.com/flights/airports"))
-                        .header("x-rapidapi-key", "ae4f671a76mshf0b5d8dd4515a3cp176dd3jsn9f9ab9d328cd")
-                        .header("x-rapidapi-host", "sky-scanner3.p.rapidapi.com")
-                        .method("GET", HttpRequest.BodyPublishers.noBody())
-                        .build();
-                HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
-
-                return ResponseEntity.ok(response.body());
-            } catch (Exception e) {
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while processing the request.");
-            }
+        if (token == null || token.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Missing, invalid, or expired Authorization token.");
         }
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized access!");
+        UserAccessInfo userAccessInfo = new UserAccessInfo();
+        userAccessInfo.setToken(token);
+
+        try {
+            UserAccessInfo accessInfo = authorizationServicePort.hasAccess("klant");
+            if (!accessInfo.hasAccess() || !accessInfo.getRole().equals("klant")) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied. User does not have the required role.");
+            }
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create("https://skyscanner89.p.rapidapi.com/flights/complete/list"))
+                    .header("x-rapidapi-key", tokenRapid)
+                    .header("x-rapidapi-host", hostRapid)
+                    .method("GET", HttpRequest.BodyPublishers.noBody())
+                    .build();
+            HttpResponse<String> response = HttpClient.newHttpClient().send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.body().contains("You are not subscribed to this API.")) {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You are not subscribed to this API.");
+            }
+
+            return ResponseEntity.ok(response.body());
+        } catch (Exception e) {
+            System.out.println("Error occurred while processing the request: " + e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred while processing the request.");
+        }
     }
 }
